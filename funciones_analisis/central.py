13f5929e5c7f77c50d60b30b2ffb94cda73f730b
Germiprogramer <llorentemunozgerman@gmail.com>
2025-03-22 22:50:18 +0100

    commit

---
+++ funciones_analisis/central.py
@@ -0,0 +1,20 @@
+def duelos_defensivos(jugador):
+    pass
+
+def intercepciones(jugador):
+    pass
+
+def pases_progresivos(jugador):
+    pass
+
+def pases_largos(jugador):
+    pass
+
+def acciones_defensivas(jugador):
+    pass
+
+def recuperaciones(jugador):
+    pass
+
+def goles(jugador):
+    pass
